69f201491f3e5813ec86ee733bd220a97a9fc85f
Jackson Daniel <jackson.daniel.17dh@gmail.com>
2021-02-14 14:39:37 -0300

    Adicionado Character

---
+++ Assets/Source/Project/Classes/CharacterClass.cs
@@ -0,0 +1,32 @@
+﻿using Framework.Classes;
+using Project.ScriptableObjects;
+using UnityEngine;
+
+namespace Project.Classes
+{
+    public class CharacterClass : ClassGeneric
+    {
+        public int hp;
+        public int atk;
+
+        public int atkBns;
+        public int diceBns;
+
+        public CharacterScriptableObject characterScriptable;
+
+        void Awake()
+        {
+            SetScriptableObject();
+        }
+
+        private void SetScriptableObject()
+        {
+            if (characterScriptable)
+            {
+                hp = characterScriptable.hp;
+                atk = characterScriptable.atk;
+                GetComponent<SpriteRenderer>().sprite = characterScriptable.sprite;
+            }
+        }
+    }
+}
---
+++ Assets/Source/Project/ScriptableObjects/CharacterScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Project.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "New Character", menuName = "ScriptableObjects/Character")]
+    public class CharacterScriptableObject : ScriptableObject
+    {
+        public string characterName;
+
+        public int hp;
+        public int atk;
+
+        public Sprite sprite;
+    }
+}
