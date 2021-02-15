348001a82db0de468a9cafbe3866b3aca6abfc07
Jackson Daniel <jackson.daniel.17dh@gmail.com>
2021-02-14 21:16:12 -0300

    Adicionado Highlight de seleção

---
+++ Assets/Source/Project/Controllers/TileMapController.cs
@@ -0,0 +1,41 @@
+﻿using Framework.Classes;
+using Framework.Controllers;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Project.Controllers
+{
+    public class TileMapController : ControllerGeneric
+    {
+        private Vector3Int lastCell;
+
+        public Tilemap tileMap;
+        public override void NewInputs(Dictionary<string, object> ipt)
+        {
+            HighlightTile((Vector3)ipt["Cursor"]);
+        }
+
+        public override void TrackObject(ClassGeneric obj)
+        {
+        }
+
+        private void HighlightTile(Vector3 position)
+        {
+            
+            var cp = tileMap.WorldToCell(position);
+            if(cp != lastCell)
+            {
+                if (lastCell != null)
+                {
+                    tileMap.SetColor(lastCell, Color.white);
+                    tileMap.SetTileFlags(lastCell , TileFlags.LockAll);
+                }
+
+                tileMap.SetTileFlags(lastCell, TileFlags.None);
+                tileMap.SetColor(cp, Color.red);
+                lastCell = cp;
+            }
+        }
+    }
+}
---
+++ Assets/Source/Project/Managers/ExtraGameManager.cs
@@ -0,0 +1,20 @@
+﻿using Framework.Gamepads;
+using Framework.Tools;
+using Project.Controllers;
+using UnityEngine;
+
+namespace Project.Manager
+{
+    public class ExtraGameManager : MonoBehaviour
+    {
+        public TileMapGenerator tileMapGenerator;
+
+        void Start()
+        {
+            var controller = new TileMapController();
+            controller.tileMap = tileMapGenerator.tileMap;
+
+            GetComponent<GamepadGeneric>().controller = controller;
+        }
+    }
+}
